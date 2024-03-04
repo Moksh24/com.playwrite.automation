1651c4f3af808bb6a7efc20786ed0c0eb8692f5d
Moksh Juneje <moksh24juneja@gmail.com>
2024-03-04 03:16:29 -0500

    Playwright_Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Playwright playwright=Playwright.create();
+        Browser browser=playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false));
+        Page page=browser.newPage();
+        page.navigate("https://www.google.com/");
+        page.close();
+        browser.close();
+        playwright.close();
+
+
+    }
+}
