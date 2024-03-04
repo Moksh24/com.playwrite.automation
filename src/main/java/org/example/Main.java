9b99768b173888827044828a2907af31256404fe
Moksh24 <moksh24juneja@gmail.com>
2024-03-04 04:58:53 -0500

    Changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,22 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.testng.annotations.Test;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
 
-        Playwright playwright=Playwright.create();
-        Browser browser=playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false));
-        Page page=browser.newPage();
-        page.navigate("https://www.google.com/");
-        page.close();
-        browser.close();
-        playwright.close();
+        @Test
+        public void invokeBrowser() {
+            Playwright playwright = Playwright.create();
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://www.facebook.com/");
+            page.close();
+            browser.close();
+            playwright.close();
+        }
 
-
-    }
 }
---
+++ src/test/java/Test123.java
@@ -0,0 +1,21 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.Test;
+
+public class Test123 {
+
+    @Test
+    public void invokeBrowser() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://www.facebook.com/");
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+
+}
