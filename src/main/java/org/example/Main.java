05727e2a7195ab9aa6d058e077ee44be692b9e35
Moksh24 <moksh24juneja@gmail.com>
2024-03-04 05:03:38 -0500

    Setting headless

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ public class Main {
         public void invokeBrowser() {
             Playwright playwright = Playwright.create();
             Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions().setHeadless(true));
+                    new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             page.navigate("https://www.facebook.com/");
             page.close();
--- src/test/java/Test123.java
+++ src/test/java/Test123.java
@@ -10,7 +10,7 @@ public class Test123 {
     public void invokeBrowser() {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false));
+                new BrowserType.LaunchOptions().setHeadless(true));
         Page page = browser.newPage();
         page.navigate("https://www.facebook.com/");
         page.close();
